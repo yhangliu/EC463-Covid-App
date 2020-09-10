6a6511aed85b25d681634824f183d0fa640ae82c
linb1 <brianlin1590@gmail.com>
2020-09-09 23:21:53 -0400

    added covid survey logic

---
+++ covid-tracker/src/SurveyData.js
@@ -0,0 +1,58 @@
+export const SurveyData = [
+    {
+        id: 0,
+        question: `Do you have a fever of 100F or feeling unusually hot?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 1,
+        question: `Do you have a new cough not related to chronic conditions?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 2,
+        question: `Are you experiencing difficulty breathing/shortness of breath?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 3,
+        question: `Do you have a sore throat?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 4,
+        question: `Are you experiencing loss of taste and/or smell?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 5,
+        question: `Have you experienced vomiting/diarrhea recently?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 6,
+        question: `Are you experiencing severe fatigue or nausea?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 7,
+        question: `Are you experiencing severe muscle aches?`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+    {
+        id: 8,
+        question: `Are you experiencing congestion or runny nose`,
+        options: [`No`, `Yes`],
+        answer: `No`
+    },
+]
+
+export default SurveyData
