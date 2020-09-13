21e6dfff0c6db593bf4313e8403c274cb344f7c0
yhangliu <37546201+yhangliu@users.noreply.github.com>
2020-09-13 00:27:59 -0400

    set up states for the api

--- covid-tracker/src/components/App.js
+++ covid-tracker/src/components/App.js
@@ -5,12 +5,12 @@ import "../css/App.css";
 import firebase from "firebase";
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY, //hid firebase api
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+    apiKey: process.env.REACT_APP_API_KEY, //hid firebase api
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 });
 
 export default class App extends Component {
--- covid-tracker/src/components/HomePage.js
+++ covid-tracker/src/components/HomePage.js
@@ -3,11 +3,27 @@ import Survey from "./Survey.js";
 import { Button } from "react-bootstrap";
 import firebase from "firebase";
 
+
+
+
+
+async function get_data() {
+    const url = "https://api.covidtracking.com/v1/states/MA/current.json?state=MA";
+    const response = await fetch(url);
+    const data = await response.json();
+    //this.setState({date:data.date});
+    this.setState({date: data.date,state: data.state, positive: data.positive, negative: data.negative, lastupdate: data.lastUpdateET})
+    console.log(data.date);
+}
+
+
 export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = { showSurvey: false };
-  }
+    }
+
+
 
   render() {
     return (
